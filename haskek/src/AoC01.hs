1057192a1ef331cd047f9beb505ede4b8876748b
~~~ <flutterlice@gmail.com>
2020-12-01 13:48:44 +0500

    [01] More improved (property) testing in haskell

--- haskek/src/AoC01.hs
+++ haskek/src/AoC01.hs
@@ -2,21 +2,24 @@ module AoC01 where
 
 import Util
 
+type AnswerGroup = [Int]
+type Product     = Int
+
 -- | Maybe finds two numbers from a list that give `target` in sum and multiplies them
-aoc01 :: Int -> [Int] -> Maybe Int
+aoc01 :: Int -> [Int] -> Maybe (AnswerGroup, Product)
 aoc01 _ [] = Nothing
 aoc01 target (x:xs) | null complements = aoc01 target xs
-                    | otherwise        = Just $ x * head complements
+                    | otherwise        = Just $ ([x, head complements], x * head complements)
   where complements = filter (==target-x) xs
 
 -- | Maybe finds `count` numbers from a list that give `target` in sum and multiplies them
-aoc01s :: Int -> Int -> [Int] -> Maybe Int
+aoc01s :: Int -> Int -> [Int] -> Maybe (AnswerGroup, Product)
 aoc01s _ _ [] = Nothing
 aoc01s count target l@(x:xs) | count < 2  = Nothing
                              | count == 2 = aoc01 target l
                              | otherwise  = case aoc01s (pred count) (target-x) xs of
                                               Nothing -> aoc01s count target xs
-                                              Just y  -> Just $ x * y
+                                              Just (ys, yp)  -> Just (x:ys, x * yp)
 
 runAoC01 input = do
   let arrToSort = parseAsInts Nothing input
--- haskek/test/AoC01_Test.hs
+++ haskek/test/AoC01_Test.hs
@@ -2,12 +2,24 @@ module AoC01_Test where
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+import Data.Maybe
 import AoC01
 
 test_aoc01 :: TestTree
 test_aoc01 = testGroup "UnitTest for AoC01" [
-    testCase "[01][S] Easy test"          $ assertEqual "" (Just 2019)     $ aoc01    2020 [2019, 1],
-    testCase "[01][S] Easy test 2"        $ assertEqual "" (Just 1020100)  $ aoc01    2020 [1010, 1010],
-    testCase "[01][S] Harder test"        $ assertEqual "" (Just 100)      $ aoc01    20   [10, 11, 9, 10],
-    testCase "[01][G] Test for gold star" $ assertEqual "" (Just 10100000) $ aoc01s 3 2020 [10, 13, 4124, 4124, 1000, 1010]
+    testCase "[01][S] False negative"     $ assertEqual "" (Just ([2019, 1], 2019)) $ aoc01 2020 [2019, 1],
+    testCase "[01][S] Easy test"          $ assertBool ""   $ satisfies aoc01      2 2020 [2019, 1],
+    testCase "[01][S] Easy test 2"        $ assertBool ""   $ satisfies aoc01      2 2020 [1010, 1010],
+    testCase "[01][S] Harder test"        $ assertBool ""   $ satisfies aoc01      2 20   [10, 11, 9, 10],
+    testCase "[01][G] Test for gold star" $ assertBool ""   $ satisfies (aoc01s 3) 3 2020 [10, 13, 4124, 4124, 1000, 1010],
+    QC.testProperty "[01][S] QuickChecking by property"   $ satisfies aoc01 2 10,
+    QC.testProperty "[01][G] QuickChecking by property 2" $ satisfies (aoc01s 3) 3
   ]
+ where satisfies f targetn targets list | length list < targetn = isNothing $ f targets list
+                                        | otherwise = case f targets list of
+                                             Nothing -> True
+                                             Just (ys, yp) ->
+                                               and [and $ ((flip elem) list) <$> ys,
+                                                    sum ys == targets,
+                                                    product ys == yp]
